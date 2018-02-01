447af8072ad213017ddbfa90541960c4a27d5007
beranemo <beranemo@gmail.com>
2018-02-01 21:29:27 +0800

    set photo api v1 resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :photos, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
+  
   resources :photos do
     collection do
       post :qq
